95da4e85de28c1f58de43058aceb0075285d3e14
inba <inba2002.p@gmail.com>
2023-02-16 20:02:16 +0530

    ran a script

--- Assignment-3/sim_nand.sh
+++ Assignment-3/sim_nand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of replacement values
-replacements=(4 5 6 7 8 9)
+replacements=(3 4 5 6 7 8 9)
 
 # Loop through the replacement values and replace them in the file
 for r in "${replacements[@]}"
